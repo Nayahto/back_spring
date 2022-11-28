a04d7e2c35e1a23532e817990ced3d229b495451
Nayahto <nayahto.147@gmail.com>
2022-11-27 22:52:30 -0300

    novo commit para o projeto java com spring boot

---
+++ projeto_back/src/test/java/com/nayahto/proj/ProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nayahto.proj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
